6a43106b6f193f4ae069119c792b52ba0039785c
Added Kina <addedk@gmail.com>
2023-12-30 17:58:16 +0100

    Remove factorial function

--- SpaceShip-Shooter/test/GameLogicTest.cpp
+++ SpaceShip-Shooter/test/GameLogicTest.cpp
@@ -2,10 +2,6 @@
 #include "../src/Ship.hpp"
 #include "doctest.h"
 
-int factorial(int number) {
-  return number > 1 ? factorial(number - 1) * number : 1;
-}
-
 TEST_CASE("Testing GameState Functionality ") {
   GameLogic::GameState gameState(10, 10);
   REQUIRE(gameState.getEnemyShips().size() == 0);
