3adfd361e50d5392ed4e74bbf5452de4e80b44ba
Added Kina <addedk@gmail.com>
2023-12-31 13:11:30 +0100

    Update projectile

--- SpaceShip-Shooter/src/Projectile.hpp
+++ SpaceShip-Shooter/src/Projectile.hpp
@@ -1,6 +1,7 @@
 #ifndef PROJECTILE_HPP
 #define PROJECTILE_HPP
 
+#include "gameEnums.hpp"
 namespace GameLogic {
 class Projectile {
 public:
@@ -9,12 +10,15 @@ public:
   int width;
   int height;
   int movementSpeed;
+  MoveDirection direction;
 
   Projectile()
-      : x_position(0), y_position(0), width(1), height(1), movementSpeed(1) {}
-  Projectile(int x_position, int y_position, int width, int height)
+      : x_position(0), y_position(0), width(1), height(1), movementSpeed(1),
+        direction(MoveDirection::DOWN) {}
+  Projectile(int x_position, int y_position, int width, int height,
+             int movementSpeed, MoveDirection direction)
       : x_position(x_position), y_position(y_position), width(width),
-        height(height), movementSpeed(1) {}
+        height(height), movementSpeed(movementSpeed), direction(direction) {}
   Projectile(const Projectile &otherProjectile) = default;
   Projectile &operator=(const Projectile &otherProjectile) = default;
   void setMovementSpeed(int newSpeed) {
