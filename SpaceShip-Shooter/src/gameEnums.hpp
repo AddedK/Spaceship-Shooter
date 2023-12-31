10b8d064dbb4bd508b89159b4ab98c90de17367b
Added Kina <addedk@gmail.com>
2023-12-31 11:53:37 +0100

    Move GameLogic enums to separate file

---
+++ SpaceShip-Shooter/src/gameEnums.hpp
@@ -0,0 +1,9 @@
+#ifndef GAME_ENUMS_HPP
+#define GAME_ENUMS_HPP
+namespace GameLogic {
+
+enum class KeyPress { LEFT, UP, RIGHT, DOWN, ENTER, SPACE, Q };
+enum class MoveDirection { LEFT, UP, RIGHT, DOWN };
+} // namespace GameLogic
+
+#endif
