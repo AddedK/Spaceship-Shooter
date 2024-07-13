e30aedee87b673098f5ce21ce82b37b0425bcef8
Added Kina <addedk@gmail.com>
2024-07-13 20:45:03 +0200

    Add back reworked gamestate test

--- SpaceShip-Shooter/test/GameLogicTest.cpp
+++ SpaceShip-Shooter/test/GameLogicTest.cpp
@@ -1,6 +1,32 @@
 #include "../src/game_logic/GameLogic.hpp"
+#include "../src/game_logic/Point.hpp"
 #include "../src/game_logic/Ship.hpp"
 #include "doctest.h"
+TEST_CASE("Testing GameState Functionality ") {
+  GameLogic::GameState gameState(60, 10, 10);
+  REQUIRE(gameState.getEnemyShips().size() == 0);
+  auto playerShip = gameState.getPlayer();
+  REQUIRE(playerShip.vertices.size() == 0); // Default constructed
+
+  SUBCASE("Test adding a player") {
+    std::vector<GameLogic::Point> playerVertices;
+    playerVertices.push_back(GameLogic::Point{10, 10});
+    playerVertices.push_back(GameLogic::Point{5, 15});
+    playerVertices.push_back(GameLogic::Point{15, 15});
+    gameState.setPlayer({playerVertices,
+                         GameLogic::GameConstants::playerInitialSpeed,
+                         GameLogic::GameConstants::playerInitialNrOfLives});
+    auto playerShip2 = gameState.getPlayer();
+    CHECK(playerShip2.vertices.size() == 3);
+    CHECK(playerShip2.lowestX == 5);
+    CHECK(playerShip2.lowestY == 10);
+    CHECK(playerShip2.highestX == 15);
+    CHECK(playerShip2.highestY == 15);
+    CHECK(playerShip2.movementSpeed ==
+          GameLogic::GameConstants::playerInitialSpeed);
+    CHECK(playerShip2.middlePositionX == 10);
+  }
+}
 
 TEST_CASE("Line intersecting function") {
   SUBCASE("Two parallel lines") {
