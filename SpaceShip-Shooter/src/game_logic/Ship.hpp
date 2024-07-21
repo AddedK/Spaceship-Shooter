3c77124230e781de29e4a0771f5f23a40673ef44
Added Kina <addedk@gmail.com>
2024-07-21 17:38:43 +0200

    Add non-impacting ship types

--- SpaceShip-Shooter/src/game_logic/Ship.hpp
+++ SpaceShip-Shooter/src/game_logic/Ship.hpp
@@ -5,6 +5,7 @@
 
 namespace GameLogic {
 
+enum class ShipType { BASIC, ADVANCED, ULTIMATE, STRIKER };
 class Ship {
 public:
   // Edge vertices for this ship. The edges form a closed shape. Two adjacent
@@ -21,16 +22,18 @@ public:
   int frameWhenLastMovedSide;
 
   double probabilityOfSideMoveRight;
+  ShipType shipType;
 
   Ship()
       : vertices(), middlePositionX(0), lowestX(0), highestX(0), lowestY(0),
         highestY(0), movementSpeed(1), nrOfLives(1),
         frameWhenLastFiredProjectile(0), frameWhenLastMovedSide(0),
-        probabilityOfSideMoveRight(0.5) {}
-  Ship(std::vector<Point> vertices, int movementSpeed, int nrOfLives)
+        probabilityOfSideMoveRight(0.5), shipType(ShipType::BASIC) {}
+  Ship(std::vector<Point> vertices, int movementSpeed, int nrOfLives,
+       ShipType shipType = ShipType::BASIC)
       : vertices(vertices), movementSpeed(movementSpeed), nrOfLives(nrOfLives),
         frameWhenLastFiredProjectile(0), frameWhenLastMovedSide(0),
-        probabilityOfSideMoveRight(0.5) {
+        probabilityOfSideMoveRight(0.5), shipType(shipType) {
     int sumX = 0;
     for (Point point : vertices) {
       sumX += point.x;
